02af818da2b71e8126edf670b05da0aacae0c1de
nicoll-douglas <dev.nicoll.douglas@gmail.com>
2025-06-15 22:03:25 +0100

    update

--- services/web/deploy.sh
+++ services/web/deploy.sh
@@ -15,7 +15,7 @@ cd $SERVICE_DIR
 
 if [ ! -f /usr/local/bin/mkcert ]; then
   echo "Installing mkcert..."
-  sudo apt install libnss3-tools
+  sudo apt install -y libnss3-tools
   curl -JLO "https://dl.filippo.io/mkcert/latest?for=linux/amd64"
   chmod +x mkcert-v*-linux-amd64
   sudo mv mkcert-v*-linux-amd64 /usr/local/bin/mkcert
