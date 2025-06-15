e1b78df5981b142380b8cd5c10dfccb43c89a2d7
nicoll-douglas <dev.nicoll.douglas@gmail.com>
2025-06-15 13:52:55 +0100

    upate deploy scripts

--- services/syncthing/client/deploy.sh
+++ services/syncthing/client/deploy.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
 SERVICE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+PARENT_DIR="/media/jiggy/Shared"
 
 # exit on error
 set -e
+
+# switch to syncthing parent directory and create necessary folders + files
+cd $PARENT_DIR
+mkdir -p syncthing/data
+mkdir -p syncthing/config
 
 # switch to directory
 cd $SERVICE_DIR
--- services/syncthing/server/deploy.sh
+++ services/syncthing/server/deploy.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
 SERVICE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+HOME_DIR="/home/jiggy"
 
 # exit on error
 set -e
 
-# switch to directory
+# switch to home directory and create necessary folders + files
+cd $HOME_DIR
+mkdir -p syncthing/data
+mkdir -p syncthing/config
+mkdir -p filebrowser
+touch filebrowser/database.db
+
+# switch to current directory
 cd $SERVICE_DIR
 
 echo "Starting syncthing and filebrowser containers..."
