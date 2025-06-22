0de0ab9db665ba6e9e7620d19accffec2f8bc5f0
nicoll-douglas <dev.nicoll.douglas@gmail.com>
2025-06-22 21:01:17 +0100

    update site configs

--- services/web/deploy.sh
+++ services/web/deploy.sh
@@ -2,7 +2,7 @@
 
 service_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 network_name="nginx-proxy-net"
-domains_file="../../config/nginx/domains"
+nginx_sites="../../config/nginx/sites"
 certs_dir="./certs"
 logs_dir="../../logs/nginx"
 
@@ -26,26 +26,27 @@ if [ -z "$domains" ]; then
   exit 1
 fi
 
-echo "Creating certs directory if it doesn't exist..."
-sudo -u "$SUDO_USER" mkdir -p $certs_dir
+sudo -u "$SUDO_USER" mkdir -vp $certs_dir
 
-for domain in $domains; do
-  cert_file="$certs_dir/$domain.pem"
-  cert_key="$certs_dir/$domain-key.pem"
+for file in $nginx_sites/*.conf; do
+  domain_base=$(basename "${file%.conf}")
+  cert_file="$certs_dir/$domain_base.pem"
+  cert_key="$certs_dir/$domain_base-key.pem"
 
   if [ ! -f $cert_file ] || [ ! -f $cert_key ]; then
-    echo "Generating TLS certificate for $domain"
+    rm -v "$certs_dir/*$domain_base*"
+
+    echo "Generating TLS certificate for $domain_base.*"
     sudo -u "$SUDO_USER" mkcert \
       -cert-file $cert_file \
       -key-file $cert_key \
-      $domain
+      $domain_base.lan $domain_base.dev
   else
-    echo "Certificate exists for $domain, skipping..."
+    echo "Certificate exists for $domain_base.*, skipping..."
   fi
 done
 
-echo "Creating logs directory if it doesn't exist..."
-sudo -u "$SUDO_USER" mkdir -p $logs_dir
+sudo -u "$SUDO_USER" mkdir -vp $logs_dir
 
 if ! sudo docker network inspect "$network_name" >/dev/null 2>&1; then
   echo "Creating external network '$network_name'..."
